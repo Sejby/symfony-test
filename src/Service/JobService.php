b2a9cd4c164eff493b761facf38b0af3f67d19bf
Sejby <janschejbal03@gmail.com>
2024-06-08 23:36:19 +0200

    strankovani, cachovani a vypis jobs

--- src/Service/JobService.php
+++ src/Service/JobService.php
@@ -7,76 +7,88 @@ use App\Model\Job;
 use Exception;
 use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class JobService
 {
-
     private $client;
     private $jobMapper;
     private $accessToken;
+    private $cache;
 
-    public function __construct(string $token, HttpClientInterface $client, JobMapper $jobMapper)
+    public function __construct(string $token, HttpClientInterface $client, JobMapper $jobMapper, CacheInterface $cache)
     {
         $this->client = $client;
         $this->jobMapper = $jobMapper;
         $this->accessToken = $token;
+        $this->cache = $cache;
     }
 
     public function fetchJobs(int $page, int $limit): array
-{
-    try {
-        $response = $this->client->request(
-            'GET',
-            'https://app.recruitis.io/api2/jobs',
-            [
-                'query' => [
-                    'page' => $page,
-                    'limit' => $limit,
-                ],
-                'headers' => [
-                    'Authorization' => 'Bearer ' . $this->accessToken,
-                ],
-            ]
-        );
+    {
+        $cacheKey = sprintf('jobs_page_%d_limit_%d', $page, $limit);
 
-        $data = $response->toArray();
+        return $this->cache->get($cacheKey, function (ItemInterface $item) use ($page, $limit) {
+            $item->expiresAfter(3600); // Nastavte dobu expirace cache na 1 hodinu
 
-        $jobs = [];
-        foreach ($data['payload'] as $jobData) {
-            $job = $this->jobMapper->map($jobData);
-            $jobs[] = $job;
-        }
+            try {
+                $response = $this->client->request(
+                    'GET',
+                    'https://app.recruitis.io/api2/jobs',
+                    [
+                        'query' => [
+                            'page' => $page,
+                            'limit' => $limit,
+                        ],
+                        'headers' => [
+                            'Authorization' => 'Bearer ' . $this->accessToken,
+                        ],
+                    ]
+                );
 
-        $total_jobs = $data['meta']['entries_total']; // Celkový počet pracovních inzerátů
+                $data = $response->toArray();
 
-        return ['jobs' => $jobs, 'total_jobs' => $total_jobs];
+                $jobs = [];
+                foreach ($data['payload'] as $jobData) {
+                    $job = $this->jobMapper->map($jobData);
+                    $jobs[] = $job;
+                }
 
-    } catch (ClientException $e) {
-        throw new Exception($e->getMessage(), $e->getCode());
+                $total_jobs = $data['meta']['entries_total']; // Celkový počet pracovních inzerátů
+
+                return ['jobs' => $jobs, 'total_jobs' => $total_jobs];
+
+            } catch (ClientException $e) {
+                throw new Exception($e->getMessage(), $e->getCode());
+            }
+        });
     }
-}
-
 
     public function fetchJobDetail(int $id): Job
     {
-        try {
-            $response = $this->client->request(
-                'GET',
-                sprintf('https://api.example.com/jobs/%d', $id),
-                [
-                    'headers' => [
-                        'Authorization' => 'Bearer ' . $this->accessToken,
-                    ],
-                ]
-            );
+        $cacheKey = sprintf('job_detail_%d', $id);
 
-            $data = $response->toArray();
-            $job = $this->jobMapper->map($data['payload']);
+        return $this->cache->get($cacheKey, function (ItemInterface $item) use ($id) {
+            $item->expiresAfter(3600); // Nastavte dobu expirace cache na 1 hodinu
 
-            return $job;
+            try {
+                $response = $this->client->request(
+                    'GET',
+                    sprintf('https://api.example.com/jobs/%d', $id),
+                    [
+                        'headers' => [
+                            'Authorization' => 'Bearer ' . $this->accessToken,
+                        ],
+                    ]
+                );
 
-        } catch (ClientException $e) {
-            throw new Exception($e->getMessage(), $e->getCode());
-        }
+                $data = $response->toArray();
+                return $this->jobMapper->map($data['payload']);
+
+            } catch (ClientException $e) {
+                throw new Exception($e->getMessage(), $e->getCode());
+            }
+        });
     }
 }
